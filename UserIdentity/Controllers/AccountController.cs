b6f6d61a95aefdb747df09efe547856dfd6685f4
Durmuş <durmusbilgec@gmail.com>
2020-06-05 12:03:23 +0300

    first commit

---
+++ UserIdentity/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserIdentity.Identity;
+using UserIdentity.Models;
+
+namespace UserIdentity.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+
+        private UserManager<ApplicationUser> userManager;
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+            userManager.PasswordValidator = new PasswordValidator()
+            {
+                RequireDigit = true,
+                RequiredLength = 7,
+                RequireLowercase = true,
+                RequireUppercase = true,
+                RequireNonLetterOrDigit = true
+            };
+            userManager.UserValidator = new UserValidator<ApplicationUser>(userManager)
+            {
+                RequireUniqueEmail = true,
+                AllowOnlyAlphanumericUserNames = false
+            };
+        }
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var user = userManager.Find(model.UserName, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Yanlış kullanıcı adı veya parola");
+                }
+                else
+                {
+                    var authManager = HttpContext.GetOwinContext().Authentication;
+                    var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+                    var authProperties = new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+
+                    };
+                    authManager.SignOut();
+                    authManager.SignIn(authProperties, identity);
+
+                    return Redirect(string.IsNullOrEmpty(returnUrl) ? "/" : returnUrl);
+                }
+            }
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+
+        
+
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ UserIdentity/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserIdentity.Identity;
+using UserIdentity.Models;
+
+namespace UserIdentity.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        
+        private UserManager<ApplicationUser> userManager;
+        public AdminController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+
+
+                var result = userManager.Create(user, model.Password);
+
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(model);
+        }
+       
+    }
+    
+}
